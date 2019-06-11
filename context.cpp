4757f71940997bcbc20dd7c6e9c5759c27af2ef4
Azegor <azegor@hotmail.de>
2019-06-11 22:06:31 +0900

    implemented assignment to struct fields

--- context.cpp
+++ context.cpp
@@ -104,7 +104,7 @@ GlobalContext::GlobalContext()
     "malloc",
     module
   );
-  mallocFunction->addFnAttr(llvm::Attribute::NoAlias);
+  //mallocFunction->addFnAttr(llvm::Attribute::NoAlias);
 }
 
 GlobalContext::~GlobalContext()
